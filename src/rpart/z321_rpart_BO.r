24007b61fbcd7503e22f039af4bca33a6d48bdd4
GustavoDenicolay <gustavo.denicolay@gmail.com>
2023-06-07 21:13:16 -0300

    z321_rpart_BO.r formato VSCode

--- src/rpart/z321_rpart_BO.r
+++ src/rpart/z321_rpart_BO.r
@@ -1,8 +1,8 @@
-#Optimizacion Bayesiana de hiperparametros de  rpart
+# Optimizacion Bayesiana de hiperparametros de  rpart
 
-#limpio la memoria
-rm( list=ls() )  #remove all objects
-gc()             #garbage collection
+# limpio la memoria
+rm(list = ls()) # remove all objects
+gc() # garbage collection
 
 require("data.table")
 require("rlist")
@@ -10,193 +10,245 @@ require("rlist")
 require("rpart")
 require("parallel")
 
-#paquetes necesarios para la Bayesian Optimization
+# paquetes necesarios para la Bayesian Optimization
 require("DiceKriging")
 require("mlrMBO")
 
 
-#Defino la  Optimizacion Bayesiana
+# Defino la  Optimizacion Bayesiana
 
-kBO_iter  <- 100   #cantidad de iteraciones de la Optimizacion Bayesiana
+# cantidad de iteraciones de la Optimizacion Bayesiana
+kBO_iter <- 100
 
-hs  <- makeParamSet(
-          makeNumericParam("cp"       , lower= -1   , upper=    0.1),
-          makeIntegerParam("minsplit" , lower=  1L  , upper= 8000L),  #la letra L al final significa ENTERO
-          makeIntegerParam("minbucket", lower=  1L  , upper= 4000L),
-          makeIntegerParam("maxdepth" , lower=  3L  , upper=   20L),
-          forbidden = quote( minbucket > 0.5*minsplit ) )             # minbuket NO PUEDE ser mayor que la mitad de minsplit
+# la letra L al final de 1L significa ENTERO
+hs <- makeParamSet(
+  makeNumericParam("cp", lower = -1, upper = 0.1),
+  makeIntegerParam("minsplit", lower = 1L, upper = 8000L),
+  makeIntegerParam("minbucket", lower = 1L, upper = 4000L),
+  makeIntegerParam("maxdepth", lower = 3L, upper = 20L),
+  forbidden = quote(minbucket > 0.5 * minsplit)
+)
+# minbuket NO PUEDE ser mayor que la mitad de minsplit
 
-ksemilla_azar  <- 102191   #cambiar por la primer semilla
+ksemilla_azar <- 102191 # cambiar por la primer semilla
 
 #------------------------------------------------------------------------------
-#graba a un archivo los componentes de lista
-#para el primer registro, escribe antes los titulos
+# graba a un archivo los componentes de lista
+# para el primer registro, escribe antes los titulos
 
-loguear  <- function( reg, arch=NA, folder="./work/", ext=".txt", verbose=TRUE )
-{
-  archivo  <- arch
-  if( is.na(arch) )  archivo  <- paste0( folder, substitute( reg), ext )
+loguear <- function(reg, arch = NA, folder = "./work/", ext = ".txt",
+                    verbose = TRUE) {
+  archivo <- arch
+  if (is.na(arch)) archivo <- paste0(folder, substitute(reg), ext)
 
-  if( !file.exists( archivo ) )  #Escribo los titulos
-  {
-    linea  <- paste0( "fecha\t", 
-                      paste( list.names(reg), collapse="\t" ), "\n" )
+  # Escribo los titulos
+  if (!file.exists(archivo)) {
+    linea <- paste0(
+      "fecha\t",
+      paste(list.names(reg), collapse = "\t"), "\n"
+    )
 
-    cat( linea, file=archivo )
+    cat(linea, file = archivo)
   }
 
-  linea  <- paste0( format(Sys.time(), "%Y%m%d %H%M%S"),  "\t",     #la fecha y hora
-                    gsub( ", ", "\t", toString( reg ) ),  "\n" )
+  # la fecha y hora
+  linea <- paste0(
+    format(Sys.time(), "%Y%m%d %H%M%S"), "\t",
+    gsub(", ", "\t", toString(reg)), "\n"
+  )
 
-  cat( linea, file=archivo, append=TRUE )  #grabo al archivo
+  # grabo al archivo
+  cat(linea, file = archivo, append = TRUE)
 
-  if( verbose )  cat( linea )   #imprimo por pantalla
+  # imprimo por pantalla
+  if (verbose) cat(linea)
 }
 #------------------------------------------------------------------------------
-#particionar agrega una columna llamada fold a un dataset que consiste en una particion estratificada segun agrupa
-# particionar( data=dataset, division=c(70,30), agrupa=clase_ternaria, seed=semilla)   crea una particion 70, 30 
-# particionar( data=dataset, division=c(1,1,1,1,1), agrupa=clase_ternaria, seed=semilla)   divide el dataset en 5 particiones
+# particionar agrega una columna llamada fold a un dataset
+#   que consiste en una particion estratificada segun agrupa
+# particionar( data=dataset, division=c(70,30),
+#  agrupa=clase_ternaria, seed=semilla)   crea una particion 70, 30
+# particionar( data=dataset, division=c(1,1,1,1,1),
+#  agrupa=clase_ternaria, seed=semilla)   divide el dataset en 5 particiones
 
-particionar  <- function( data, division, agrupa="", campo="fold", start=1, seed=NA )
-{
-  if( !is.na( seed)  )   set.seed( seed )
+particionar <- function(data, division, agrupa = "", campo = "fold",
+                        start = 1, seed = NA) {
+  if (!is.na(seed)) set.seed(seed)
 
-  bloque  <- unlist( mapply(  function(x,y) { rep( y, x ) }, division, seq( from=start, length.out=length(division) )  ) )
+  bloque <- unlist(mapply(
+    function(x, y) {
+      rep(y, x)
+    }, division,
+    seq(from = start, length.out = length(division))
+  ))
 
-  data[ , (campo) :=  sample( rep( bloque, ceiling(.N/length(bloque))) )[1:.N],
-           by= agrupa ]
+  data[, (campo) := sample(rep(bloque, ceiling(.N / length(bloque))))[1:.N],
+    by = agrupa
+  ]
 }
 #------------------------------------------------------------------------------
-#fold_test  tiene el numero de fold que voy a usar para testear, entreno en el resto de los folds
-#param tiene los hiperparametros del arbol
+# fold_test  tiene el numero de fold que voy a usar para testear,
+#  entreno en el resto de los folds
+# param tiene los hiperparametros del arbol
 
-ArbolSimple  <- function( fold_test, data, param )
-{
-  #genero el modelo
-  modelo  <- rpart("clase_ternaria ~ .", 
-                   data= data[ fold != fold_test, ],  #entreno en todo MENOS el fold_test que uso para testing
-                   xval= 0,
-                   control= param )
+ArbolSimple <- function(fold_test, data, param) {
+  # genero el modelo
+  # entreno en todo MENOS el fold_test que uso para testing
+  modelo <- rpart("clase_ternaria ~ .",
+    data = data[fold != fold_test, ],
+    xval = 0,
+    control = param
+  )
 
-  #aplico el modelo a los datos de testing
-  prediccion  <- predict( modelo, 
-                          data[ fold==fold_test, ],  #aplico el modelo sobre los datos de testing
-                          type= "prob")   #quiero que me devuelva probabilidades
+  # aplico el modelo a los datos de testing
+  # aplico el modelo sobre los datos de testing
+  # quiero que me devuelva probabilidades
+  prediccion <- predict(modelo,
+    data[fold == fold_test, ],
+    type = "prob"
+  )
 
-  prob_baja2  <- prediccion[, "BAJA+2"]  #esta es la probabilidad de baja
+  # esta es la probabilidad de baja
+  prob_baja2 <- prediccion[, "BAJA+2"]
 
-  #calculo la ganancia
-  ganancia_testing  <- data[ fold==fold_test ][ prob_baja2 > 1/40,  
-                                                sum( ifelse( clase_ternaria=="BAJA+2", 117000, -3000 ) )] 
+  # calculo la ganancia
+  ganancia_testing <- data[fold == fold_test][
+    prob_baja2 > 1 / 40,
+    sum(ifelse(clase_ternaria == "BAJA+2",
+      117000, -3000
+    ))
+  ]
 
-  return( ganancia_testing )  #esta es la ganancia sobre el fold de testing, NO esta normalizada
+  # esta es la ganancia sobre el fold de testing, NO esta normalizada
+  return(ganancia_testing)
 }
 #------------------------------------------------------------------------------
 
-ArbolesCrossValidation  <- function( data, param, qfolds, pagrupa, semilla )
-{
-  divi  <- rep( 1, qfolds )  # generalmente  c(1, 1, 1, 1, 1 )  cinco unos
+ArbolesCrossValidation <- function(data, param, qfolds, pagrupa, semilla) {
+  # generalmente  c(1, 1, 1, 1, 1 )  cinco unos
+  divi <- rep(1, qfolds)
 
-  particionar( data, divi, seed=semilla, agrupa=pagrupa )  #particiono en dataset en folds
+  # particiono en dataset en folds
+  particionar(data, divi, seed = semilla, agrupa = pagrupa)
 
-  ganancias  <- mcmapply( ArbolSimple, 
-                          seq(qfolds), # 1 2 3 4 5
-                          MoreArgs= list( data, param), 
-                          SIMPLIFY= FALSE,
-                          mc.cores= 1 )   #se puede subir a qfolds si posee Linux o Mac OS
+  ganancias <- mcmapply(ArbolSimple,
+    seq(qfolds), # 1 2 3 4 5
+    MoreArgs = list(data, param),
+    SIMPLIFY = FALSE,
+    mc.cores = qfolds
+  )
 
-  data[ , fold := NULL ]
+  data[, fold := NULL]
 
-  #devuelvo la primer ganancia y el promedio
-  ganancia_promedio  <- mean( unlist( ganancias ) )   #promedio las ganancias
-  ganancia_promedio_normalizada  <- ganancia_promedio * qfolds  #aqui normalizo la ganancia
-  
-  return( ganancia_promedio_normalizada )
+  # devuelvo la primer ganancia y el promedio
+  # promedio las ganancias
+  ganancia_promedio <- mean(unlist(ganancias))
+  # aqui normalizo la ganancia
+  ganancia_promedio_normalizada <- ganancia_promedio * qfolds
+
+  return(ganancia_promedio_normalizada)
 }
 #------------------------------------------------------------------------------
-#esta funcion solo puede recibir los parametros que se estan optimizando
-#el resto de los parametros, lamentablemente se pasan como variables globales
+# esta funcion solo puede recibir los parametros que se estan optimizando
+# el resto de los parametros, lamentablemente se pasan como variables globales
 
-EstimarGanancia  <- function( x )
-{
-   GLOBAL_iteracion  <<-  GLOBAL_iteracion + 1
+EstimarGanancia <- function(x) {
+  GLOBAL_iteracion <<- GLOBAL_iteracion + 1
 
-   xval_folds  <- 5
-   ganancia  <- ArbolesCrossValidation( dataset,
-                                        param= x, #los hiperparametros del arbol
-                                        qfolds= xval_folds,  #la cantidad de folds
-                                        pagrupa= "clase_ternaria",
-                                        semilla= ksemilla_azar )
+  xval_folds <- 5
+  # param= x los hiperparametros del arbol
+  # qfolds= xval_folds  la cantidad de folds
+  ganancia <- ArbolesCrossValidation(dataset,
+    param = x,
+    qfolds = xval_folds,
+    pagrupa = "clase_ternaria",
+    semilla = ksemilla_azar
+  )
 
-   #logueo 
-   xx  <- x
-   xx$xval_folds  <-  xval_folds
-   xx$ganancia  <- ganancia
-   xx$iteracion <- GLOBAL_iteracion
-   loguear( xx,  arch= archivo_log )
+  # logueo
+  xx <- x
+  xx$xval_folds <- xval_folds
+  xx$ganancia <- ganancia
+  xx$iteracion <- GLOBAL_iteracion
+  loguear(xx, arch = archivo_log)
 
-   return( ganancia )
+  return(ganancia)
 }
 #------------------------------------------------------------------------------
-#Aqui empieza el programa
+#------------------------------------------------------------------------------
+# Aqui empieza el programa
 
-setwd("~/buckets/b1/")   #Establezco el Working Directory
+# Establezco el Working Directory
+setwd("~/buckets/b1/")
 
-#cargo los datos
-dataset  <- fread("./datasets/dataset_pequeno.csv")
-#trabajo solo con los datos con clase, es decir 202107
-dataset  <- dataset[ clase_ternaria!= "" ]
-
-
-#creo la carpeta donde va el experimento
-# HT  representa  Hiperparameter Tuning
-dir.create( "./exp/",  showWarnings = FALSE ) 
-dir.create( "./exp/HT3210/", showWarnings = FALSE )
-setwd("./exp/HT3210/")   #Establezco el Working Directory DEL EXPERIMENTO
+# cargo los datos
+dataset <- fread("./datasets/dataset_pequeno.csv")
+# trabajo solo con los datos con clase, es decir 202107
+dataset <- dataset[clase_ternaria != ""]
 
 
-archivo_log  <- "HT321.txt"
-archivo_BO   <- "HT321.RDATA"
+# creo la carpeta donde va el experimento
+#  HT  representa  Hiperparameter Tuning
+dir.create("./exp/", showWarnings = FALSE)
+dir.create("./exp/HT3210/", showWarnings = FALSE)
 
-#leo si ya existe el log, para retomar en caso que se se corte el programa
-GLOBAL_iteracion  <- 0
+# Establezco el Working Directory DEL EXPERIMENTO
+setwd("./exp/HT3210/")
 
-if( file.exists(archivo_log) )
-{
- tabla_log  <- fread( archivo_log )
- GLOBAL_iteracion  <- nrow( tabla_log )
+
+archivo_log <- "HT321.txt"
+archivo_BO <- "HT321.RDATA"
+
+# leo si ya existe el log
+#  para retomar en caso que se se corte el programa
+GLOBAL_iteracion <- 0
+
+if (file.exists(archivo_log)) {
+  tabla_log <- fread(archivo_log)
+  GLOBAL_iteracion <- nrow(tabla_log)
 }
 
 
 
-#Aqui comienza la configuracion de la Bayesian Optimization
+# Aqui comienza la configuracion de la Bayesian Optimization
 
-funcion_optimizar  <- EstimarGanancia
+funcion_optimizar <- EstimarGanancia
 
-configureMlr( show.learner.output= FALSE)
+configureMlr(show.learner.output = FALSE)
 
-#configuro la busqueda bayesiana,  los hiperparametros que se van a optimizar
-#por favor, no desesperarse por lo complejo
-obj.fun  <- makeSingleObjectiveFunction(
-              fn=       funcion_optimizar,
-              minimize= FALSE,   #estoy Maximizando la ganancia
-              noisy=    TRUE,
-              par.set=  hs,
-              has.simple.signature = FALSE
-             )
+# configuro la busqueda bayesiana,
+#  los hiperparametros que se van a optimizar
+# por favor, no desesperarse por lo complejo
+# minimize= FALSE estoy Maximizando la ganancia
+obj.fun <- makeSingleObjectiveFunction(
+  fn = funcion_optimizar,
+  minimize = FALSE,
+  noisy = TRUE,
+  par.set = hs,
+  has.simple.signature = FALSE
+)
 
-ctrl  <- makeMBOControl( save.on.disk.at.time= 600,  save.file.path= archivo_BO)
-ctrl  <- setMBOControlTermination(ctrl, iters= kBO_iter )
-ctrl  <- setMBOControlInfill(ctrl, crit= makeMBOInfillCritEI())
+ctrl <- makeMBOControl(
+  save.on.disk.at.time = 600,
+  save.file.path = archivo_BO
+)
 
-surr.km  <- makeLearner("regr.km", predict.type= "se", covtype= "matern3_2", control= list(trace= TRUE))
+ctrl <- setMBOControlTermination(ctrl, iters = kBO_iter)
+ctrl <- setMBOControlInfill(ctrl, crit = makeMBOInfillCritEI())
 
-#inicio la optimizacion bayesiana
-if( !file.exists( archivo_BO ) ) {
+surr.km <- makeLearner("regr.km",
+  predict.type = "se",
+  covtype = "matern3_2", control = list(trace = TRUE)
+)
 
-  run  <- mbo( fun=     obj.fun, 
-               learner= surr.km,
-               control= ctrl)
-
-} else  run  <- mboContinue( archivo_BO )   #retomo en caso que ya exista
-
+# inicio la optimizacion bayesiana
+if (!file.exists(archivo_BO)) {
+  run <- mbo(
+    fun = obj.fun,
+    learner = surr.km,
+    control = ctrl
+  )
+} else {
+  run <- mboContinue(archivo_BO)
+}
+# retomo en caso que ya exista
