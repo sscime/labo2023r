d3c264ac8b6a714444a64139b306df01b2b3ccf0
sscime <scimesilvia@gmail.com>
2023-06-09 18:04:10 +0000

    auto commit 1       20230609 180410

--- src/rpart/111_traintest_estratificado.r
+++ src/rpart/111_traintest_estratificado.r
@@ -23,7 +23,7 @@ particionar <- function(data, division, agrupa = "", campo = "fold", start = 1, 
 #------------------------------------------------------------------------------
 
 # Aqui se debe poner la carpeta de la computadora local
-setwd("X:\\gdrive\\austral2023r\\") # Establezco el Working Directory
+setwd("~/buckets/b1") # Establezco el Working Directory
 
 # cargo los datos
 dataset <- fread("./datasets/dataset_pequeno.csv")
