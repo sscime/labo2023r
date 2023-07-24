0399061548d2a754492b60322bc8ef890bd628fa
sscime <scimesilvia@gmail.com>
2023-07-23 23:41:42 -0300

    'cambios2bb'

---
+++ src/workflow-semillerio/z701_RUN_correr_workflow2.r
@@ -0,0 +1,16 @@
+# Corrida general del workflow Semillerio
+
+options(error = function() {
+  traceback(20)
+  options(error = NULL)
+  stop("exiting after script error")
+})
+
+
+# corrida de cada paso del workflow
+source("~/labo2023r/src/workflow-semillerio/z711_CA_reparar_dataset2.r")
+source("~/labo2023r/src/workflow-semillerio/z721_DR_corregir_drifting2.r")
+source("~/labo2023r/src/workflow-semillerio/z731_FE_historia2.r")
+source("~/labo2023r/src/workflow-semillerio/z741_TS_training_strategy2.r")
+source("~/labo2023r/src/workflow-semillerio/z751_HT_lightgbm2.r")
+source("~/labo2023r/src/workflow-semillerio/z771_ZZ_final_semillerio2.r")
