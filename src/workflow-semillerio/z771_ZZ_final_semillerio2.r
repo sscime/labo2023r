7f5decaf88f8d1c6a2f37110eb97d550198dc386
sscime <scimesilvia@gmail.com>
2023-07-29 17:19:22 -0300

    'salvavida'

--- src/workflow-semillerio/z771_ZZ_final_semillerio2.r
+++ src/workflow-semillerio/z771_ZZ_final_semillerio2.r
@@ -30,7 +30,7 @@ PARAM$modelos_rank <- c(1)
 # cantidad de semillas a utilizar en el semillerio,
 #  podrian ser 50 o 100 para mayor estabilidad
 #  semillerio <- 1  es el ZZ_final  tradicional
-PARAM$semillerio <- 40
+PARAM$semillerio <- 5
 
 # se utiliza para generar el vector de  PARAM$semillerio  semillas
 PARAM$semilla_primos <- 556687
